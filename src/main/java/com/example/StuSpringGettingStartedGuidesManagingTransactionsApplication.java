783c40bcb1911c9d2d02c904b715417a79169912
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-11-15 22:00:01 +0900

    first commit

---
+++ src/main/java/com/example/StuSpringGettingStartedGuidesManagingTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuSpringGettingStartedGuidesManagingTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuSpringGettingStartedGuidesManagingTransactionsApplication.class, args);
+	}
+
+}
